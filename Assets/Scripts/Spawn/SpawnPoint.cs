92b0282e31bdd0e6889914d9a5038bea76ba8bff
TheRedOne <lobachevartem1997@mail.ru>
2020-12-02 14:09:22 +0300

    spawn at a specific position

--- Assets/Scripts/Spawn/SpawnPoint.cs
+++ Assets/Scripts/Spawn/SpawnPoint.cs
@@ -8,15 +8,12 @@ namespace Spawn
     {
         [SerializeField] protected GameObject prefab;
         [SerializeField] private Color color;
-        [SerializeField] protected float spawnRadius = 3f;
 
         public void Spawn(LevelSettings levelSettings)
         {
             var position = transform.position;
-            var randomSpot = new Vector2(Random.Range(position.x - spawnRadius, position.x + spawnRadius), 
-                Random.Range(position.y - spawnRadius, position.y + spawnRadius));
             var go = Instantiate(prefab);
-            go.transform.position = new Vector3(randomSpot.x, randomSpot.y, 13.3f);
+            go.transform.position = new Vector3(position.x, position.y, 13.3f);
             AfterSpawn(go, levelSettings);
         }
 
@@ -25,9 +22,7 @@ namespace Spawn
         private void OnDrawGizmos()
         {
             Gizmos.color = color;
-            var position = transform.position;
-            Gizmos.DrawCube(position, Vector3.one);
-            Gizmos.DrawWireSphere(position, spawnRadius);
+            Gizmos.DrawCube(transform.position, Vector3.one);
         }
     }
 }
